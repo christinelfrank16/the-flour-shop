1ad93ee899427eceb5eae8a9ae9ced7a37932d66
Christine Frank <christine.braun13@gmail.com>
2019-09-27 09:50:46 -0700

    Finalize products classes

--- Bakery/Products.cs
+++ Bakery/Products.cs
@@ -5,25 +5,34 @@ namespace Bakery
     public class Product
     {
         public string Type { get; set; }
-        public double Cost { get; set; }
-        public Product(string type, double cost)
+        public double DefaultCost { get; set; }
+        public int[] Sale { get; set; }
+        public Product(string type, double defaultCost)
         {
             Type = type;
-            Cost = cost;
+            DefaultCost = defaultCost;
+            Sale = new int[3]; // buyCount, getCount, totalSaleCost
         }
     }
 
     public class Bread : Product{
-        public Bread():base("Bread", 2.00){
+        public bool IsGlutenFree { get; set; }
+        public bool IsSliced { get; set; }
 
+        public Bread(bool glutenFree, bool isSliced):base("Bread", 2.00){
+            IsGlutenFree = glutenFree;
+            IsSliced = isSliced;
         }
     }
 
     public class Pastry : Product
     {
-        public Pastry() : base("Pastry", 1.00)
+        public bool IsSavory { get; set; }
+        public string Shape { get; set; }
+        public Pastry(bool isSavory, string shape) : base("Pastry", 1.00)
         {
-
+            IsSavory = isSavory;
+            Shape = shape;
         }
     }
 }
